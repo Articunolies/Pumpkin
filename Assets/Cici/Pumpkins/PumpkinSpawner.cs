966c4c84ea15f0ba98c4d47dc4b7abb634c0f56e
Christopher Raheb <craheb123@gmail.com>
2024-10-12 16:01:51 -0700

    Vine stuff

--- Assets/Cici/Pumpkins/PumpkinSpawner.cs
+++ Assets/Cici/Pumpkins/PumpkinSpawner.cs
@@ -7,6 +7,7 @@ public class PumpkinSpawner : MonoBehaviour
     public GameObject background;
     public GameObject pumpkin;
     public GameObject vine;
+    public float vineOffset = 0.5f;
     public Sprite[] pumpkinSprites;
     public Sprite[] vineSprites; // 1a, 1b, 2a, 2b, 3a, 3b, 4a, 4b, 5a, 5b, 6a, 6b, 7a, 7b
     // A list to hold the spawned pumpkins
@@ -28,49 +29,13 @@ public class PumpkinSpawner : MonoBehaviour
 
             // Instantiate the pumpkin and add it to the list
             GameObject spawnedPumpkin = Instantiate(pumpkin, position, Quaternion.identity);
-            spawnedPumpkin.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = pumpkinSprites[Random.Range(0, pumpkinSprites.Length)];
+            spawnedPumpkin.GetComponent<SpriteRenderer>().sprite = pumpkinSprites[Random.Range(0, pumpkinSprites.Length)];
             spawnedPumpkins.Add(spawnedPumpkin); // Add to the list
 
             // Pick a random pair of vine sprites and instantiate them
-            GameObject spawnedVine  = Instantiate(vine, position, Quaternion.identity);
-            GameObject spawnedVine2 = Instantiate(vine, position, Quaternion.identity);
-
-            // THE WORST CODE IVE EVER WRITTEN
+            GameObject spawnedVine  = Instantiate(vine, position - new Vector3(0,vineOffset,0), Quaternion.identity);
             int vineIndex = Random.Range(0, 7);
-            switch(vineIndex){
-                case 0:
-                    spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[0];
-                    spawnedVine2.GetComponent<SpriteRenderer>().sprite = vineSprites[1];
-                    break;
-                case 1:
-                    spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[2];
-                    spawnedVine2.GetComponent<SpriteRenderer>().sprite = vineSprites[3];
-                    break;
-                case 2:
-                    spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[4];
-                    spawnedVine2.GetComponent<SpriteRenderer>().sprite = vineSprites[5];
-                    break;
-                case 3:
-                    spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[6];
-                    spawnedVine2.GetComponent<SpriteRenderer>().sprite = vineSprites[7];
-                    break;
-                case 4:
-                    spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[8];
-                    spawnedVine2.GetComponent<SpriteRenderer>().sprite = vineSprites[9];
-                    break;
-                case 5:
-                    spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[10];
-                    spawnedVine2.GetComponent<SpriteRenderer>().sprite = vineSprites[11];
-                    break;
-                case 6:
-                    spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[12];
-                    spawnedVine2.GetComponent<SpriteRenderer>().sprite = vineSprites[13];
-                    break;
-                case 7:
-                    spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[14];
-                    spawnedVine2.GetComponent<SpriteRenderer>().sprite = vineSprites[15];
-                    break;
-            }
+            spawnedVine.GetComponent<SpriteRenderer>().sprite = vineSprites[vineIndex];
         }
 
         // Find the AIController in the scene and pass the entire list of pumpkins
