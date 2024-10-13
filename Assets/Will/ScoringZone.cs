50e888113badcc9cb87e51064b6f9e5e52d26983
William Klunder <wklunder@ucsc.edu>
2024-10-12 17:40:12 -0700

    interaction getters and setters, fixed scoring stuff and added scoring controller

--- Assets/Will/ScoringZone.cs
+++ Assets/Will/ScoringZone.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ScoringZone : MonoBehaviour
 {
-    static int score = 0;
     private List<GameObject> scoredItems = new List<GameObject>();
+    [SerializeField] private GameObject Player;
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Pumpkin" && !scoredItems.Contains(other.gameObject))
         {
+            if(Player.GetComponent<PlayerInteraction>().GetPumpkin()){
+                GameObject playerPumpkin = Player.GetComponent<PlayerInteraction>().GetPumpkin();
+                if(playerPumpkin == other.gameObject){
+                    Player.GetComponent<PlayerInteraction>().SetHasPumpkin(false);
+                }
+            }
             scoredItems.Add(other.gameObject); 
             Destroy(other.gameObject);
-            score += 1;
-            Debug.Log("Score = " + score);
+            ScoreController.instance.AddScore(1);
+            Debug.Log("Score = " + ScoreController.instance.GetScore());
         }
     }
 }
