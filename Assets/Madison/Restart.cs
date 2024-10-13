91915bda1cd040eb84226d403d79239a932e832b
mli254 <mli254@ucsc.edu>
2024-10-12 21:31:33 -0700

    added environment

--- Assets/Madison/Restart.cs
+++ Assets/Madison/Restart.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Restart : MonoBehaviour
 {
+    public GameObject SceneController;
+    ScoreController scoreController;
+
+    void Start() 
+    {
+        SceneController = GameObject.Find("Score Controller");
+        scoreController = SceneController.GetComponent<ScoreController>();
+    }
+
     public void RestartGame()
     {
         // Scene 0 -> MainScene: Main Gameplay Scene
         // Scene 1 -> GameOver: Game Over Scene
         SceneManager.LoadScene(0);
+        scoreController.SetScore(0);
     }
 }
